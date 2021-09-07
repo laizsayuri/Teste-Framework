1b11fa8083703b818316cb6ac3bf9b73ce5f6273
laizsayuri <laizsayuri@gmail.com>
2021-09-06 21:46:40 -0300

    Integração da tabela de posts com a API

---
+++ Teste Framework/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace Teste_Framework.Models
+{
+    public class Post
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Titulo { get; set; }
+
+        [JsonPropertyName("body")]
+        public string Conteudo { get; set; }
+    }
+}
--- Teste Framework/Pages/Posts.cshtml.cs
+++ Teste Framework/Pages/Posts.cshtml.cs
@@ -1,16 +1,31 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Teste_Framework.Models;
 
 namespace Teste_Framework.Pages
 {
     public class PostsModel : PageModel
     {
-        public void OnGet()
+        public List<Post> Posts { get; set; }
+
+        public async Task<IActionResult> OnGet()
         {
+            Posts = new List<Post>();
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+            var result = await httpClient.GetAsync("posts");
+            var resultString = await result.Content.ReadAsStringAsync();
+
+            Posts = JsonSerializer.Deserialize<List<Post>>(resultString);
+
+            return Page();
         }
     }
 }
